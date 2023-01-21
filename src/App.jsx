e51a85e2ead4ca471d32853601b2f7da33023c71
Leonardo Banze <leonardobanze37@gmail.com>
2023-01-21 09:02:47 +0200

    add p

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Bot from "./components/Bot"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Hireme from "./components/Hireme"
+import Project from "./components/Projects"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+
+function App() {
+
+  return (
+   <div>
+     <Navbar/>
+    <Bot/>
+    <Hero/>
+    <About/>
+    <Skills/>
+    <Hireme/>
+    <Project/>
+    <Contact/>
+    <Footer/>
+   </div>
+  )
+}
+
+export default App
